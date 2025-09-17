be844739ab5b859d6b988e16a52ec40bda5b88fb
AnikaTahshin <anikatahsinmeem@gmail.com>
2025-09-17 19:52:37 +0600

    navbar on process

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -10,13 +10,26 @@ const Dashboard = () => {
     const [isOpen, setIsOpen] = useState(false)
   return (
     <>
-    <div className="flex min-h-[100vh] h-100vh overflow-y-hidden">
+    <div className="flex">
+        <div className="flex min-h-[100vh] h-[100vh] overflow-y-hidden">
          <Sidebar isOpen={isOpen} setIsOpen={setIsOpen} />
     </div>
-    {/* <Navbar />
-   
-    <MyChart />
-    <Main /> */}
+    
+
+    {
+        isOpen && 
+        <div onClick={() => setIsOpen(false)} className="fixed inset-0 bg-black/20 z-40 lg:hidden"></div>          
+    }
+
+    <div className="flex-1 bg-gray-100 h-[100vh] min-h-[100vh] overflow-y-scroll">
+         <Navbar setIsOpen={setIsOpen} />
+
+         <div className="main-content p-5 bg-[#f3f8fe]">
+             <Main />
+         </div>
+
+    </div>
+    </div>
     </>
   )
 };
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,8 +1,29 @@
-import React from 'react'
+"use client";
+import React, { useState } from 'react'
+import { MdSearch } from "react-icons/md";
 
-const Navbar = () => {
+const Navbar = ({setIsOpen}:any) => {
+  const [isUserMenuOpen, setIsUserMenuOpen] = useState(false)
+  const [isNotificationOpen, setIsNotificationOpen] = useState(false);
+
+  const toggleUserMenu = () => {
+    setIsUserMenuOpen(!isUserMenuOpen);
+    setIsNotificationOpen(false);
+  }
+   const toggleNotification = () => {
+    setIsNotificationOpen(!isUserMenuOpen);
+    setIsUserMenuOpen(false);
+  }
   return (
-    <div>Navbar</div>
+    <>
+    <div className="bg-white h-[90px] shadow-lg flex justify-between items-center gap-3 px-[2%]">
+      <div className="search-box border border-[#dfe0e4] relative h-[45px] hidden lg:flex items-center rounded-full w-70 outline-none">
+        <input type="text" placeholder='Search' className='h-full w-full ps-4 outline-none' />
+        <MdSearch className='absolute bg-[#006dca] text-white right-0.5 p-3 rounded-[50%]' />
+
+      </div>
+    </div>
+    </>
   )
 }
 
