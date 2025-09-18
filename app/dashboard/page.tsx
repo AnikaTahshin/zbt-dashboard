59da0ddc8a82d8f9f5564cac8bafeeeb4900c9db
AnikaTahshin <anikatahsinmeem@gmail.com>
2025-09-18 17:58:46 +0600

    not found page done

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,37 +1,35 @@
-'use client';
+"use client";
 import Main from "@/components/Main";
-import MyChart from "@/components/MyChart";
 import Navbar from "@/components/Navbar";
 import Sidebar from "@/components/Sidebar";
 import React, { useState } from "react";
 
-
 const Dashboard = () => {
-    const [isOpen, setIsOpen] = useState(false)
+  const [isOpen, setIsOpen] = useState(false);
   return (
     <>
-    <div className="flex">
+      <div className="flex">
         <div className="flex min-h-[100vh] h-[100vh] overflow-y-hidden">
-         <Sidebar isOpen={isOpen} setIsOpen={setIsOpen} />
-    </div>
-    
+          <Sidebar isOpen={isOpen} setIsOpen={setIsOpen} />
+        </div>
 
-    {
-        isOpen && 
-        <div onClick={() => setIsOpen(false)} className="fixed inset-0 bg-black/20 z-40 lg:hidden"></div>          
-    }
+        {isOpen && (
+          <div
+            onClick={() => setIsOpen(false)}
+            className="fixed inset-0 bg-black/20 z-40 lg:hidden"
+          ></div>
+        )}
 
-    <div className="flex-1 bg-gray-100 h-[100vh] min-h-[100vh] overflow-y-scroll">
-         <Navbar setIsOpen={setIsOpen} />
+        <div className="flex-1 bg-gray-100 h-[100vh] min-h-[100vh] overflow-y-scroll">
+          <Navbar setIsOpen={setIsOpen} />
 
-         <div className="main-content p-5 bg-[#f3f8fe]">
-             <Main />
-         </div>
-
-    </div>
-    </div>
+          <div className="main-content p-5 bg-[#f3f8fe]">
+            <Main />
+          </div>
+        </div>
+      </div>
     </>
-  )
+  );
 };
 
 export default Dashboard;
--- components/MyChart.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const MyChart = () => {
-  return (
-    <div>MyChart</div>
-  )
-}
-
-export default MyChart
